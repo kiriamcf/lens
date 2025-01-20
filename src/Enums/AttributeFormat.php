3201248616a88602bed12f078c1310197b044ba7
kiriamcf <kiriamcf@gmail.com>
2025-01-21 00:30:46 +0100

    added more attribute formats

--- src/Enums/AttributeFormat.php
+++ src/Enums/AttributeFormat.php
@@ -6,11 +6,18 @@ namespace Kiriamcf\Lens\Enums;
 
 /**
  * @internal
+ * 
+ * @see https://laravel.com/docs/11.x/blade#escaping-attribute-rendering
+ * @see https://vuejs.org/guide/essentials/class-and-style.html
+ * @see https://alpinejs.dev/directives/bind
  */
 enum AttributeFormat
 {
     case REGULAR;
-    case BINDING;
+    case GENERIC_BINDING;
+    case LARAVEL_ESCAPED;
+    case VUE_BINDING;
+    case ALPINEJS_BINDING;
 
     /**
      * Get the HTML representation of the attribute format.
@@ -19,7 +26,10 @@ enum AttributeFormat
     {
         return match ($this) {
             self::REGULAR => '{attribute}',
-            self::BINDING => ':{attribute}',
+            self::GENERIC_BINDING => ':{attribute}',
+            self::LARAVEL_ESCAPED => '::{attribute}',
+            self::VUE_BINDING => 'v-bind:{attribute}',
+            self::ALPINEJS_BINDING => 'x-bind:{attribute}',
         };
     }
 }
